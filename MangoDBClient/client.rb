75064039b62f98976b52be26d9371e4b66d932d7
Sushant <sushant.dinesh94@gmail.com>
2014-11-15 00:12:13 +0530

    Update commit

--- MangoDBClient/client.rb
+++ MangoDBClient/client.rb
@@ -2,40 +2,44 @@ require "net/http"
 require "json"
 
 class DB
-    attr_accessor :port, :ip, :uri
-    def insert hash
-        Net::HTTP.post_form URI(@uri + "/put"), "hash" => JSON.dump(hash) 
-    end
+  attr_accessor :port, :ip, :uri
+  def insert hash
+    Net::HTTP.post_form(URI(@uri + "/put"), "hash" => JSON.dump(hash))
+  end
 
-    def delete key
-        Net::HTTP.post_form URI(@uri + "/delete"), "key" => key
-    end
+  def delete key
+    Net::HTTP.post_form(URI(@uri + "/delete"), "key" => key)
+  end
 
-    def get key
-        response = Net::HTTP.get URI(@uri + "/get/#{key}")
-        response.strip!
-        if response != 'null'
-            JSON.parse response
-            response
-        else
-            nil
-        end
+  def get key
+    response = Net::HTTP.get(URI(@uri + "/get/#{key}"))
+    response.strip!
+    if response != 'null'
+      JSON.parse response
+      response
+    else
+      nil
     end
+  end
 
-    def initialize(ip, port)
-        @ip = ip
-        @port = port
-        @uri = "http://" + @ip.to_s + ":" + @port.to_s
-    end
-
+  def initialize(ip, port)
+    @ip = ip
+    @port = port
+    @uri = "http://" + @ip.to_s + ":" + @port.to_s
+  end
 end
 
 binding = binding()
 
 while true    
-    print "> "
-    input = STDIN.gets.chomp!
-    result = eval input, binding
-    puts "-> " + result.to_s
+  print "MangoDB > "
+  input = STDIN.gets.chomp!
+  begin
+    result = eval(input, binding)
+    puts "  -> " + result.to_s
+  rescue Exception => e
+    break if input == 'exit'
+    puts e.class.to_s + ": " + e.message.to_s
+  end
 end
 
--- MangoDBServer/main.rb
+++ MangoDBServer/main.rb
@@ -4,37 +4,45 @@
 #   -> Implement custom Key
 #   -> Implement custom Value
 #   -> Implement string class
+
 require 'json'
 require 'sinatra'
 require "sinatra/reloader" 
 class MangoDB < Sinatra::Base
-    enable :logging
-    configure :development do 
-        register Sinatra::Reloader
-    end
-    temporaryStore = {}
+  enable :logging
+  configure :development do 
+    register Sinatra::Reloader
+  end
 
-    get '/' do
-        "MangoDB Running on port #{Sinatra::Application.port}"
-    end
+  temporaryStore = {}
 
-    get '/get/:key' do
-        content_type :json
-        ret = temporaryStore[params[:key].to_s]
-        JSON.dump temporaryStore 
-    end
+  # Show present configuration of MangoDB.
+  get '/' do
+    "MangoDB Running on port #{Sinatra::Application.port}"
+  end
 
-    post '/put' do
-        hash = JSON.parse params["hash"]
-        logger.info hash.inspect
-        hash.each { |k,v| temporaryStore[k.to_s] = v }
-        "OK"
-    end
+  get '/get/:key' do
+    content_type :json
+    ret = temporaryStore[params[:key].to_s]
+    JSON.dump temporaryStore 
+  end
 
-    post '/delete' do
-        temporaryStore.delete(params[:key])
-        "OK"
-    end
+  post '/put' do
+    hash = JSON.parse params["hash"]
+    logger.info hash.inspect
+    hash.each { |k,v| temporaryStore[k.to_s] = v }
+    "OK"
+  end
+
+  post '/delete' do
+    temporaryStore.delete(params[:key])
+    "OK"
+  end
+
+  # Function to test if the server is alive.
+  get '/ping' do
+    "pong"
+  end
 
 end
 
